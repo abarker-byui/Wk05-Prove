6d1577aec54aa909e016059ca5d04db17cc4e139
abarker-byui <bar17115@byui.edu>
2019-05-22 20:28:07 -0700

    Part II and Stretch

--- app/src/main/java/com/example/wk05prove/DisplayScriptureActivity.java
+++ app/src/main/java/com/example/wk05prove/DisplayScriptureActivity.java
@@ -2,11 +2,20 @@ package com.example.wk05prove;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayScriptureActivity extends AppCompatActivity {
+    private String _book;
+    private int _chapter;
+    private int _verse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +23,30 @@ public class DisplayScriptureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_scripture);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView viewScripture = (TextView)findViewById(R.id.textViewScripture);
-        viewScripture.setText(message);
+        _book = intent.getStringExtra(getString(R.string.book_key));
+        _chapter = intent.getIntExtra(getString(R.string.chapter_key), 0);
+        _verse = intent.getIntExtra(getString(R.string.verse_key), 0);
+        String scripture = String.format("%s %d:%d", _book, _chapter, _verse);
+
+        Log.d("Display","Received intent with " + scripture);
+
+        TextView viewScripture = findViewById(R.id.textViewScripture);
+        viewScripture.setText(scripture);
+    }
+
+    public void saveScripture(View view) {
+        String scripture = String.format("%s %d:%d", _book, _chapter, _verse);
+        Log.d("Save", "Saving scripture " + scripture);
+
+        Context context = getBaseContext();
+        SharedPreferences preferences = context.getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor perfEditor = preferences.edit();
+        perfEditor.putString("BOOK", _book);
+        perfEditor.putInt("CHAPTER", _chapter);
+        perfEditor.putInt("VERSE", _verse);
+        perfEditor.apply();
+
+        Toast successToast = Toast.makeText(context, getString(R.string.success_text), Toast.LENGTH_LONG);
+        successToast.show();
     }
 }
--- app/src/main/java/com/example/wk05prove/MainActivity.java
+++ app/src/main/java/com/example/wk05prove/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.wk05prove;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.wk05prove.MESSAGE";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,14 +21,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendScripture(View view) {
-        Intent intent = new Intent(this, DisplayScriptureActivity.class);
         EditText txtBook = (EditText)findViewById(R.id.editBook);
         EditText txtChapter = (EditText)findViewById(R.id.editChapter);
         EditText txtVerse = (EditText)findViewById(R.id.editVerse);
-        String message = txtBook.getText().toString() + " " +
-                txtChapter.getText().toString() + ":" +
-                txtVerse.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+
+        String book = txtBook.getText().toString();
+        int chapter = Integer.parseInt(txtChapter.getText().toString());
+        int verse = Integer.parseInt(txtVerse.getText().toString());
+
+        Log.d("Main", String.format("About to create intent with %s %d:%d", book, chapter, verse));
+
+        Intent intent = new Intent(this, DisplayScriptureActivity.class);
+        intent.putExtra(getString(R.string.book_key), book);
+        intent.putExtra(getString(R.string.chapter_key), chapter);
+        intent.putExtra(getString(R.string.verse_key), verse);
         startActivity(intent);
+    }
+
+    public void loadScripture(View view) {
+        Log.d("Load", "Loading scripture");
+
+        EditText txtBook = (EditText)findViewById(R.id.editBook);
+        EditText txtChapter = (EditText)findViewById(R.id.editChapter);
+        EditText txtVerse = (EditText)findViewById(R.id.editVerse);
+
+        Context context = getBaseContext();
+        SharedPreferences preferences = context.getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
+
+        String book = preferences.getString("BOOK", "");
+        Integer chapter = preferences.getInt("CHAPTER", 0);
+        Integer verse = preferences.getInt("VERSE", 0);
+
+        if (book == "" || chapter.equals(0) || verse.equals(0))
+        {
+            Toast toast = Toast.makeText(context, R.string.load_failure, Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+
+        txtBook.setText(book);
+        txtChapter.setText(chapter.toString());
+        txtVerse.setText(verse.toString());
     }
 }
